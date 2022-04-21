5ed14199e7253acd10a34686a1ca801e4c75167f
CaruBardd <diego35andres199@gmail.com>
2022-04-21 15:51:28 -0500

    Integración de productos con Input y ngFor

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../../models/product.model'
 
 @Component({
   selector: 'app-product',
@@ -11,5 +12,7 @@ export class ProductComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @Input('productInList') product!: Product;
 
 }
