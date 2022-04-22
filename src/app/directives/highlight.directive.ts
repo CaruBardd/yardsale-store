eb5056b8f46d53118d73271c3aa5816ac5c435ca
CaruBardd <diego35andres199@gmail.com>
2022-04-22 00:00:29 -0500

    definicion y aplicacion de directiva appHighlight

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,10 +1,32 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  /*
+  Esta directiva modificará al HTML Element de manera que cuando:
+    - El mouse entre en el area se ejecute onMouseEnter(),
+    - El mouse salga del area, se ejecute onMouseLeave().
+  */
+
+  // El HostListener contiene el evento que lo ejecuta.
+  // onMouseEnter() convertirá el background del elemento en rojo.
+  @HostListener('mouseenter') onMouseEnter() {
+    this.element.nativeElement.style.backgroundColor = 'red'
+  }
+
+  // onMouseLeave eliminará el background del elemento.
+  @HostListener('mouseleave') onMouseLeave() {
+    this.element.nativeElement.style.backgroundColor = ''
+  }
+
+  constructor(
+    //Inyección de ElementRef
+    private element: ElementRef
+  ) {
+    // this.element.nativeElement.style.backgroundColor = 'red';
+   }
 
 }
