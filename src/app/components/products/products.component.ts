7707f80266debca2b8f97fae4275065ce5065fbc
CaruBardd <diego35andres199@gmail.com>
2022-04-21 22:49:17 -0500

    fetch web service data

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { StoreService } from 'src/app/services/store.service';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -11,42 +12,21 @@ export class ProductsComponent implements OnInit {
 
   constructor(
     //Inyección de dependencias
-    private storeService: StoreService
+    private storeService: StoreService,
+    private productsService: ProductsService
   ) {
     this.myShoppingCart = this.storeService.getShoppingCart();
   }
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.productsService.getAllProducts()
+      .subscribe(data => {
+        console.log(data)
+      });
   }
 
   myShoppingCart: Product[] = [];
   total: number = 0;
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      img: './assets/images/toy.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      img: './assets/images/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      img: './assets/images/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      img: './assets/images/books.jpg'
-    },
-  ]
+  products: Product[] = []
 
   onAddToShoppingCart(product: Product) {
     this.storeService.addToShoppingCart(product);
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(
+    // Inyección de Servicio de HttpClient
+    private http: HttpClient
+  ) { }
+
+  getAllProducts() {
+    return this.http.get('https://fakestoreapi.com/products')
+  }
+
 }
